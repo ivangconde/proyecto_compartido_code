791e42631493a38440968ddd2d1b0d5a42d0c03f
ivan <ivan@ivan.com>
2018-04-24 08:47:29 +0200

    primera version de vehiculo.java

---
+++ vehiculo.java
@@ -0,0 +1,28 @@
+public class Vehiculo {
+	private String color;
+	private int puertas;
+	private int asientos;
+
+	public Vehiculo (String color, int puertas, int asientos) {
+		//Cada alumno deberá crear este constructor
+		
+		
+		
+	}
+	
+	public Vehiculo () {
+		
+	}
+	
+	public String getColor(){
+		return this.color;
+	}
+	public int getPuertas(){
+		return this.puertas;
+	}
+	public int getAsientos(){
+		return this.asientos;
+	}
+	
+}
+
